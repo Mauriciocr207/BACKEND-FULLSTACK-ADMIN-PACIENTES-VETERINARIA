81dda82f933263738aac0d85de2cbf5253fec57d
Mauricio CR <mauriciocr123@outlook.com>
2024-04-07 10:40:15 -0600

    correction

--- Config/database.js
+++ Config/database.js
@@ -1,17 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-const result = dotenv.config({silent: true});
-console.log(result);
-if(result.error) {
-    console.log('error en dotenv');
-    console.log(result.error);
-    console.log(process.env.MONGO_URI);
-}
-
+dotenv.config({silent: true});
+console.log(process.env.PORT);
 export const DB = async () => {
     try {
-        console.log(process.env.MONGO_URI);
         const connection = await mongoose.connect(process.env.MONGO_URI);
         const {connection: {host, port}} = connection;
         const url = `${host}:${port}`;
